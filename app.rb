072be29c4182588dcecc465f38227be9244336fd
Teerth Patel <teerth1205@gmail.com>
2020-03-06 01:03:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  
+
+configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
+
+get '/' do 
+  erb :index
+end 
 end
