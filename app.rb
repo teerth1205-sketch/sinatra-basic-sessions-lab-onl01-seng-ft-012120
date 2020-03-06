b885af3efb14026d61daf1256ac5780397714f10
Teerth Patel <teerth1205@gmail.com>
2020-03-06 01:19:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 configure do
-  enable :sessions
+  enable :session
   set :session_secret, "secret"
 end
 
@@ -12,8 +12,8 @@ get '/' do
 end 
 
 post '/checkout' do 
-  sessions["item"] = params["item"]
-  @sessions = sessions 
+  session["item"] = params["item"]
+  @session = session
   
   erb :checkout 
 end 
